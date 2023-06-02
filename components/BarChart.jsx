ca47b9a82c82901ab7b19f9a3554bd5c73ea0c3a
Pranay Salve <64769068+pranaysalve@users.noreply.github.com>
2023-06-03 01:44:58 +0530

    updates

---
+++ components/BarChart.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const BarChart = () => {
+  const [chartData, setChartData] = useState({
+    datasets: [],
+  });
+
+  const [chartOptions, setChartOptions] = useState({});
+
+  useEffect(() => {
+    setChartData({
+      labels: ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"],
+      datasets: [
+        {
+          label: "Sales $ (Year 2020)",
+          data: [300, 500, 1000, 5000, 24182, 17842, 22475],
+          borderColor: "rgb(23, 12, 235)",
+          backgroundColor: "rgb(23, 12, 235, 0.4)",
+        },
+        {
+          label: "Sales $ (Year 2021)",
+          data: [18127, 22201, 1990, 17938, 24182, 17842, 22475],
+          borderColor: "rgb(53, 162, 235)",
+          backgroundColor: "rgb(53, 162, 235, 0.4)",
+        },
+      ],
+    });
+    setChartOptions({
+      plugins: {
+        legend: {
+          position: "bottom",
+        },
+        title: {
+          display: true,
+          text: "Daily Revenue",
+        },
+      },
+      maintainAspectRatio: false,
+      responsive: true,
+    });
+  }, []);
+
+  return (
+    <>
+      <div className="w-full md:col-span-2 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white">
+        <Bar data={chartData} options={chartOptions} />
+      </div>
+    </>
+  );
+};
